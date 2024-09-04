df36118b54b17fdaa4abd24375f73e8c1dcfad89
sainikhila <sainikhila1999@gmail.com>
2024-09-04 19:58:15 +0530

    Update app.ts
    Added error handler to show the proper error details instead of throwing exception

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,52 @@
 
 require("dotenv").config();
-import express, { Response as ExResponse, Request as ExRequest } from "express";
+import express, { Response as ExResponse, Request as ExRequest, NextFunction } from "express";
 import { RegisterRoutes } from "../build/routes";
 import swaggerUi from "swagger-ui-express";
-
+import { ValidateError } from "tsoa";
 import * as bodyParser from "body-parser";
 import { DbConnection } from "./db/utils/connection.db";
+
 export const app = express();
 
 
 DbConnection.initConnection().then(() => {
     DbConnection.setAutoReconnect();
 
-
     app.use(bodyParser.urlencoded({
         extended: true,
     }));
+
     app.use(bodyParser.json());
+
+    const options = { explorer: true };
 
     app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
         return res.send(
-            swaggerUi.generateHTML(await import("../build/swagger.json"))
+            swaggerUi.generateHTML(await import("../build/swagger.json"), options)
         );
     });
 
     RegisterRoutes(app);
+
+    const errorHandler = (err: unknown, req: ExRequest, res: ExResponse, next: NextFunction) => {
+        if (err instanceof ValidateError) {
+            console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+            return res.status(422).json({
+                message: "Validation Failed",
+                details: err?.fields,
+            });
+        }
+        if (err instanceof Error) {
+            return res.status(500).json({
+                message: "Internal Server Error",
+            });
+        }
+
+        next();
+    };
+
+    app.use(errorHandler);
+
 
 });
