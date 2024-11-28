aa73e584df79fe9466b58028925c18f2a223edf9
sainikhila <sainikhila1999@gmail.com>
2024-11-28 08:14:19 +0530

    Updated Cors

--- src/app.ts
+++ src/app.ts
@@ -26,6 +26,22 @@ DbConnection.initConnection().then(() => {
         );
     });
 
+    app.use(async function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Credentials", "true");
+        res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+        res.header("Access-Control-Expose-Headers", "Content-Length");
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Accept, Authorization, Content-Type, X-Requested-With, Range"
+        );
+        if (req.method === "OPTIONS") {
+            return res.sendStatus(200);
+        } else {
+            return next();
+        }
+    });
+
     RegisterRoutes(app);
 
     const errorHandler = (err: unknown, req: ExRequest, res: ExResponse, next: NextFunction) => {
